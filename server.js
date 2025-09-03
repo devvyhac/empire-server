36e33708a27258a7188646fed809df1456131f1a
Ismail Abdulqudus <ismailabdulqudus063@gmail.com>
2025-09-03 13:03:58 -0700

    Updated the server.js to set Cors origin to based the appropriate environment be it production or development.

--- server.js
+++ server.js
@@ -14,7 +14,10 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin:
+      process.env.NODE_ENV === "production"
+        ? process.env.FRONTEND
+        : "http://localhost:5173",
     credentials: true,
   })
 );
